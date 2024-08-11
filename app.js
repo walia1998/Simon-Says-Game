0803402f06c2ba864e2bab0826ee6b9ea6b1f2bf
Ashish Walia <waliaashu1998@gmail.com>
2024-08-11 13:15:45 +0530

    some changes

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-let gameSeq =[];
-let userSeq =[];
+let gameSeq = [];
+let userSeq = [];
 
-let btns = [ "red", "yellow" , "green", "blue"];
+let btns = ["red", "yellow", "green", "blue"];
 
 let started = false;
 let level = 0;
 
-let h2 =document.querySelector('h2');
+let h2 = document.querySelector('h2');
 
-//sound effect--------------------------
+// Sound effects
 let endSound = document.querySelector(".endSound");
 let gameOverSound = function () {
     endSound.play();
 }
+
 let btnsSound = document.querySelector(".btnSound")
 let btnSound = function () {
     btnsSound.play();
 }
+
 let sbtnSound = document.querySelector(".sbtnSound")
 let startbtnSound = function () {
     sbtnSound.play();
 }
 
-
-//start game----------------
+// Start/End game
 let startbtn = document.querySelector(".start"); 
-startbtn.addEventListener("click",function(){        
-    if(started == false){
-        console.log("game is started");
+startbtn.addEventListener("click", function() {        
+    if (!started) {
+        console.log("Game is started");
         started = true;   
         startbtn.innerText = "End"; 
-        startbtn.style.backgroundColor = "red"   
+        startbtn.style.backgroundColor = "red";
+        document.querySelector("body").style.backgroundColor = "rgb(224, 220, 255)"; // Reset background when starting
         levelUp();
         startbtnSound();
-               
-    }else{
-        startbtn.style.backgroundColor = "#5dbea3"   
-        startbtn.innerText = "Start";
-        startbtnSound();
-        reset()
+    } else {
+        console.log("Game is ended");
+        reset();
     }
 });
 
-
-//flaxh buttons & level up ----------------------
-function gameFlash(btn){
+// Flash buttons & level up
+function gameFlash(btn) {
+    if (!started) return; // Prevent flashing if the game is not started
     btn.classList.add("flash");
     setTimeout(() => {
         btn.classList.remove("flash");
     }, 200);
 }
 
-function userFlash(btn){
+function userFlash(btn) {
+    if (!started) return; // Prevent user flash if the game is not started
     btn.classList.add("userFlash");
     setTimeout(() => {
         btn.classList.remove("userFlash");
@@ -59,92 +59,104 @@ function userFlash(btn){
     btnSound();
 }
 
-function levelUp(){
+function levelUp() {
+    if (!started) return; // Stop if the game is not started
     userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
     let randIdx = Math.floor(Math.random() * 4);
-
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-    // console.log(randIdx);
-    // console.log(randColor);
-    // console.log(randBtn);
     gameSeq.push(randColor);
-    console.log(gameSeq);
-
+    console.log("Game Sequence:", gameSeq);
     gameFlash(randBtn);
 }
+
 let currlevel = 0;
-function checkAns(idx){
-    // console.log(`curr level : ${level}`);
-    if(userSeq[idx]===gameSeq[idx]){
-        // console.log("same value");
-        if(userSeq.length == gameSeq.length){
+function checkAns(idx) {
+    if (!started) return; // Stop if the game is not started
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length === gameSeq.length) {
             setTimeout(() => levelUp(), 1000);
         }
-    }
-    else{
+    } else {
         h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press Start Button To Start`;
-        document.querySelector("body").style.backgroundColor  = "red";
+        document.querySelector("body").style.backgroundColor = "red";
         startbtn.style.backgroundColor = "#5dbea3";   
-
         gameOverSound();
 
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor  = "black";
+        // Flashing background colors
+        setTimeout(() => { 
+            document.querySelector("body").style.backgroundColor = "black"; 
+            console.log("Background color changed to black");
         }, 100);
-        // document.querySelector("body").style.backgroundColor  = "red";
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor  = "red";
+        setTimeout(() => { 
+            document.querySelector("body").style.backgroundColor = "red"; 
+            console.log("Background color changed to red");
         }, 200);
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor  = "black";
+        setTimeout(() => { 
+            document.querySelector("body").style.backgroundColor = "black"; 
+            console.log("Background color changed to black");
         }, 300);
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor  = "red";
+        setTimeout(() => { 
+            document.querySelector("body").style.backgroundColor = "red"; 
+            console.log("Background color changed to red");
         }, 400);
-        setTimeout(() => {
-            document.querySelector("body").style.backgroundColor  = "black";
+        setTimeout(() => { 
+            document.querySelector("body").style.backgroundColor = "black"; 
+            console.log("Background color changed to black");
         }, 500);
 
         let highScore = document.querySelector('h1.highscore');
-        
-        if(currlevel >= level){
+        if (currlevel >= level) {
             highScore.innerText = `Your High Score is ${currlevel}.`;
-        }else{
+        } else {
             highScore.innerText = `Your High Score is ${level}.`;
             currlevel = level;            
         }
-        reset();
+
+        // Call reset with a delay to allow visual feedback
+        setTimeout(() => {
+            reset();
+            console.log("Game reset and background color reset to initial");
+        }, 600);
     }
 }
 
-//button event listeners ------------------------
-function btnPress(){
-    // console.log("btn was pressed"); 
-    // console.log(this);
+// Button event listeners
+function btnPress() {
+    if (!started) return; // Stop if the game is not started
     let btn = this;
     userFlash(btn);
-    // let userColor = this.id;
     let userColor = this.getAttribute("id");
-    console.log(userColor);
+    console.log("Button pressed:", userColor);
     userSeq.push(userColor);
-    console.log(userSeq);
-    
-    checkAns(userSeq.length-1);
-}
-let allBtns = document.querySelectorAll('.btn');
-for (const btn of allBtns) {
-    btn.addEventListener("click",btnPress);
+    console.log("User sequence:", userSeq);
+    checkAns(userSeq.length - 1);
 }
 
-function reset(){
+let allBtns = document.querySelectorAll('.btn');
+for (const btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    console.log("Resetting the game...");
     started = false;
     gameSeq = [];
     userSeq = [];
     level = 0;
-    startbtn.innerText = "Start"
+    startbtn.innerText = "Start";
+    startbtn.style.backgroundColor = "#5dbea3";
+    h2.innerText = "Press Start Button To Play";
+    document.querySelector("body").style.backgroundColor = "rgb(224, 220, 255)"; // Reset the background color
 
-    // highScore.innerText ="";  
+    // Optionally, you can disable button interactions here
+    for (const btn of allBtns) {
+        btn.removeEventListener("click", btnPress);
+    }
+    // Re-enable the button listeners
+    for (const btn of allBtns) {
+        btn.addEventListener("click", btnPress);
+    }
 }
